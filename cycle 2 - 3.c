3920f9a2e59caa4a145cbde129c262d6cd2f281e
192211902 <122469116+192211902@users.noreply.github.com>
2023-02-06 09:48:40 +0530

    Create cycle 2 - 3.c

---
+++ cycle 2 - 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+main()
+{
+	int a,b,c,d;
+	printf("Enter The first Number(P):");
+	scanf("%d",&a);
+	printf("Enter The second number(Q):");
+	scanf("%d",&b);
+	printf("Enter The Digit(R):");
+	scanf("%d",&c);
+	for(d=a;d<=b;d++)
+	{
+		if(d%10!=c)
+		{
+			printf("%d",d);
+			if(d<b)
+			{
+				printf(",");
+			}
+		}
+	}
+}
+Footer
+© 2023 GitHub, Inc.
